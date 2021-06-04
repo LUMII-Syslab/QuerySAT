cca3f9b6e3afa152d26af71d93096a9e701ab6cd
Emīls Ozoliņš <ozolinsemils@gmail.com>
2021-06-04 14:30:57 +0300

    Lower var count in generalization

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def evaluate_variable_generalization(model):
     results_file = get_valid_file("gen_variables_size_result.txt")
 
     lower_limit = 10
-    upper_limit = 410
+    upper_limit = 110
     step = 10
 
     for var_count in range(lower_limit, upper_limit, step):
